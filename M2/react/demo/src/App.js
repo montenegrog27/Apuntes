e54d3b186b43797578e8329b690eff2dd5a34b4e
German Montenegro <112520841+montenegrog27@users.noreply.github.com>
2023-03-13 20:11:02 -0300

    react

---
+++ M2/react/demo/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div>
+        <div>
+        <hr></hr>
+          <p>Nombre:German Montenegro</p>
+          <p>Edad:23</p>
+          <hr></hr>
+        </div>
+        <div>
+        <hr></hr>
+        <p>Nombre: Pedro Casillas</p>
+        <p>Edad:28</p>
+        <hr></hr>
+        </div>
+        <div>
+        <hr></hr>
+        <p>Nombre: Michele Margan</p>
+        <p>Edad:30</p>
+        <hr></hr>
+        </div>
+
+       </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ M2/react/demo/src/components/ListaPersonas/ListaPersonas.jsx
@@ -0,0 +1,25 @@
+const ListaPersonas = ()=>{
+    <div>
+    <div>
+    <hr></hr>
+      <p>Nombre:German Montenegro</p>
+      <p>Edad:23</p>
+      <hr></hr>
+    </div>
+
+    <div>
+    <hr></hr>
+    <p>Nombre: Pedro Casillas</p>
+    <p>Edad:28</p>
+    <hr></hr>
+    </div>
+    
+    <div>
+    <hr></hr>
+    <p>Nombre: Michele Margan</p>
+    <p>Edad:30</p>
+    <hr></hr>
+    </div>
+
+   </div>
+}
